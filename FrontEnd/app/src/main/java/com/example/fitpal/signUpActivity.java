7e9ed015fbe829920fa80094335204a65b660d7f
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-11 14:51:01 +0300

    Adding text view with on click (Intent from signUp to LogIn)

--- FrontEnd/app/src/main/java/com/example/fitpal/signUpActivity.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/signUpActivity.java
@@ -3,16 +3,37 @@ package com.example.fitpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class signUpActivity extends AppCompatActivity {
-    Button btn;
+    Button button;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
 
+        button = (Button) findViewById(R.id.signup_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signUpActivity.this, logInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        text = (TextView) findViewById(R.id.logIN);
+        text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signUpActivity.this, logInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
