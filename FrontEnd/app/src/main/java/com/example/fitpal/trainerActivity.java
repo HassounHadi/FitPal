316558e236947ad8782e0a31b6882eede1f86643
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-11 14:13:44 +0300

    Creating the rest of the Activities to create intents

--- FrontEnd/app/src/main/java/com/example/fitpal/trainerActivity.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/trainerActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class reserveActivity extends AppCompatActivity {
+public class trainerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reserve);
+        setContentView(R.layout.personal_trainers);
     }
 }
