3a82c5e6844209690cd75e1819ded18fd2bd1036
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-11 06:20:27 +0300

    Page-4 Spinner-1 Done

--- FrontEnd/app/src/main/java/com/example/fitpal/Home.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/Home.java
@@ -3,12 +3,33 @@ package com.example.fitpal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Home extends AppCompatActivity {
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reserve);
+
+        spinner1 = findViewById(R.id.spinner_access);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.access, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        //spinner1.setOnItemSelectedListener(this);
     }
+
+    /*@Override
+    public void onItemSelected(AdapterView<?> adapterView , View view , int i , long l){
+        String choice = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView){
+
+    }*/
 }
