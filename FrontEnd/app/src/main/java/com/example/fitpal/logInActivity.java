cbab35e114333f892f4163f2255e48727d9fe467
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-11 14:00:50 +0300

    Adding Intent (LogIn to SignUp)

--- FrontEnd/app/src/main/java/com/example/fitpal/logInActivity.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/logInActivity.java
@@ -2,24 +2,49 @@ package com.example.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class Home extends AppCompatActivity {
+public class logInActivity extends AppCompatActivity {
     private Spinner spinner1;
     private Spinner spinner2;
     private Spinner spinner3;
 
+    Button button;
+    TextView sign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plan);
+        setContentView(R.layout.log_in);
 
-        spinner1 = findViewById(R.id.spinner_goal);
+        button = (Button) findViewById(R.id.login);
+        sign = (TextView) findViewById(R.id.signUp);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(logInActivity.this, signUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+            }
+        });
+
+
+
+    /*    spinner1 = findViewById(R.id.spinner_goal);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.goal, android.R.layout.simple_spinner_item);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter1);
+        spinner1.setAdapter(adapter1);*/
 
 /*      spinner1 = findViewById(R.id.spinner_access);
         ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,R.array.access, android.R.layout.simple_spinner_item);
---
+++ FrontEnd/app/src/main/java/com/example/fitpal/signUpActivity.java
@@ -0,0 +1,18 @@
+package com.example.fitpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class signUpActivity extends AppCompatActivity {
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
+    }
+}
