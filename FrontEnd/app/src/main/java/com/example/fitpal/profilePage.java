f8a1adfae00c1203e097064eaaeab2fa9ab424c3
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-27 10:46:55 +0300

    Page-7 Adding Logo-Button + Setting intent for transition from Page-7 to Page-1

--- FrontEnd/app/src/main/java/com/example/fitpal/profilePage.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/profilePage.java
@@ -2,13 +2,27 @@ package com.example.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class profilePage extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        logo = (ImageView) findViewById(R.id.logo_btn);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(profilePage.this ,homeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
