2baebdb9a7581569013d6523297f47f520b2bd99
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-04-27 10:50:42 +0300

    Adding intent for transition from Page-5 to Page-1

--- FrontEnd/app/src/main/java/com/example/fitpal/trainerActivity.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/trainerActivity.java
@@ -2,13 +2,27 @@ package com.example.fitpal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class trainerActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.personal_trainers);
+
+        logo = (ImageView) findViewById(R.id.logo_btn);
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(trainerActivity.this ,homeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
