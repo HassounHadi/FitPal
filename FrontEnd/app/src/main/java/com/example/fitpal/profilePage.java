53b4f48589bd9d2626323a0d562c28d9c4dd6e87
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-05-04 18:27:42 +0300

    Page-7 Adding Intent for Transition from Page-7 to Page-2

--- FrontEnd/app/src/main/java/com/example/fitpal/profilePage.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/profilePage.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class profilePage extends AppCompatActivity {
 
     ImageView logo;
+    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,15 @@ public class profilePage extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(profilePage.this ,homeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(profilePage.this, logInActivity.class);
                 startActivity(intent);
             }
         });
