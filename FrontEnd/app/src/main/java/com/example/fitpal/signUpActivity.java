4a9a64610080ca0fcc895f64f38c2f5e1d04ffec
Hadi Hassoun <Hassounhadi8@gmail.com>
2022-05-05 23:15:14 +0300

    Removing previous front to back link

--- FrontEnd/app/src/main/java/com/example/fitpal/signUpActivity.java
+++ FrontEnd/app/src/main/java/com/example/fitpal/signUpActivity.java
@@ -27,77 +27,9 @@ public class signUpActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     EditText c_password;
-    String F_Name_Holder, L_Name_Holder, EmailHolder, PasswordHolder;
-    Boolean CheckEditText ;
-    ProgressDialog progressDialog;
-    HashMap<String,String> hashMap = new HashMap<>();
-    String finalResult ;
-    String HttpURL = "http://localhost/phpmyadmin/index.php?route=/table/structure&db=fitpal&table=user";
-    HttpParse httpParse = new HttpParse();
 
 
 
-    public void CheckEditTextIsEmptyOrNot(){
-
-        F_Name_Holder = fname.getText().toString();
-        L_Name_Holder = lname.getText().toString();
-        EmailHolder = email.getText().toString();
-        PasswordHolder = password.getText().toString();
-
-
-        if(TextUtils.isEmpty(F_Name_Holder) || TextUtils.isEmpty(L_Name_Holder) || TextUtils.isEmpty(EmailHolder) || TextUtils.isEmpty(PasswordHolder))
-        {
-            CheckEditText = false;
-        }
-        else {
-            CheckEditText = true ;
-        }
-
-    }
-
-    public void UserRegisterFunction(final String F_Name, final String L_Name, final String email, final String password){
-
-        class UserRegisterFunctionClass extends AsyncTask<String,Void,String> {
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-
-                progressDialog = ProgressDialog.show(signUpActivity.this,"Loading Data",null,true,true);
-            }
-
-            @Override
-            protected void onPostExecute(String httpResponseMsg) {
-
-                super.onPostExecute(httpResponseMsg);
-
-                progressDialog.dismiss();
-
-                Toast.makeText(signUpActivity.this,httpResponseMsg.toString(), Toast.LENGTH_LONG).show();
-
-            }
-
-            @Override
-            protected String doInBackground(String... params) {
-
-                hashMap.put("f_name",params[0]);
-
-                hashMap.put("L_name",params[1]);
-
-                hashMap.put("email",params[2]);
-
-                hashMap.put("password",params[3]);
-
-                finalResult = httpParse.postRequest(hashMap, HttpURL);
-
-                return finalResult;
-            }
-        }
-
-        UserRegisterFunctionClass userRegisterFunctionClass = new UserRegisterFunctionClass();
-
-        userRegisterFunctionClass.execute(F_Name,L_Name,email,password);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -108,25 +40,6 @@ public class signUpActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                // Checking whether EditText is Empty or Not
-                CheckEditTextIsEmptyOrNot();
-
-                if(CheckEditText){
-
-                    // If EditText is not empty and CheckEditText = True then this block will execute.
-
-                    UserRegisterFunction(F_Name_Holder,L_Name_Holder, EmailHolder, PasswordHolder);
-                    Intent intent = new Intent(signUpActivity.this, logInActivity.class);
-                    startActivity(intent); 
-
-                }
-                else {
-
-                    // If EditText is empty then this block will execute .
-                    Toast.makeText(signUpActivity.this, "Please fill all form fields.", Toast.LENGTH_LONG).show();
-
-                }
 
 
             }
